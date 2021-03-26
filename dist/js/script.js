8def9e9d130a050974ea68cc73ca60150db7685a
Vasja <qwertyqaz181207@gmail.com>
2021-03-27 01:09:49 +0200

    uncorect modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,4 @@
 
-
-
-// Modal
-$('[data-modal=thanks]').on('click', function() {
-    $('.overlay, #thanks').fadeIn('slow');
-});
-$('[data-modal=consultation]').on('click', function() {
-    $('.overlay, #consultation').fadeIn('slow');
-});
-$('.modal__close').on('click', function() {
-    $('.overlay, #consultation, #thanks').fadeOut('slow');
-});
-// $('[data-modal=detail]').on('click', function() {
-//     $('.overlay, #detail').fadeIn('slow');
-// });
-
-$('[data-modal=detail]').each(function(i) {
-    $(this).on('click', function() {
-        $('#detail .modal__subtitle').text($('.card__title').eq(i).text())
-        $('.overlay, #detail').fadeIn('slow');
-    })
-})
-
 // Burger 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
@@ -43,37 +20,9 @@ if (menuLinks.length > 0) {
             }
         });
     });
-
 }
 
-// const menuLinks = document.querySelectorAll('.menu__link[href]');
-// if (menuLinks.length > 0) {
-//     menuLinks.forEach(menuLink => {
-//         menuLink.addEventListener("click", onMenuLinkClick);
-//     });
-
-//     function onMenuLinkClick(e) {
-//         const menuLink = e.target;
-//         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-//             const gotoBlock = document.querySelector(menuLink.dataset.goto);
-//             const gotoBlockValue = gotoBlock.getBoundingClientReact().top + pageYOffset - document.querySelector('header').offsetHeight;
-           
-//             if(iconMenu.classList.contains('active')){
-//                 document.body.classList.remove('_lock');
-//                 iconMenu.classList.remove('_active');
-//                 menuBody.classList.remove('_active');
-//             }
-           
-//             window.scrollTo ({
-//                 top: gotoBlockValue,
-//                 behavior: "smooth"
-//             })
-//             e.preventDefault();
-//         }
-//     }
-// }
-
- $(document).ready(function(){
+$(document).ready(function(){
     $(".menu").on("click","a", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
@@ -81,3 +30,174 @@ if (menuLinks.length > 0) {
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+
+
+// Modal
+
+const btn__calls = document.querySelectorAll('.btn__call');
+const btn__promo = document.querySelector('.btn__promo');
+const btns__send = document.querySelectorAll('.btn__send');
+const consultation = document.querySelector('#consultation');
+const thanks = document.querySelector('#thanks');
+const overlay = document.querySelector('.overlay');
+const modal__closes = document.querySelectorAll('.modal__close');
+const detail = document.querySelector('#detail');
+const сalculation = document.querySelector('#сalculation');
+
+
+if (modal__closes.length > 0) {
+    modal__closes.forEach(modal__close => {
+        modal__close.addEventListener("click", () => {
+            overlay.style.display = "none";
+            consultation.style.display = "none";
+            thanks.style.display = "none";
+            detail.style.display = "none";
+            сalculation.style.display = "none";
+        });
+    });
+}
+
+// if (btns__send.length > 0) {
+//     btns__send.forEach(btn__send => {
+//         btn__send.addEventListener("click", () => {
+//             thanks.style.display = "block";
+//             detail.style.display = "none";
+//             consultation.style.display = "none";
+//             сalculation.style.display = "none";
+//             overlay.style.display = "block";
+//         });
+//     });
+// }
+
+if (btn__calls.length > 0) {
+    btn__calls.forEach(btn__call => {
+        btn__call.addEventListener("click", () => {
+            сalculation.style.display = "none";
+            thanks.style.display = "none";
+            detail.style.display = "none";
+            consultation.style.display = "block";
+            overlay.style.display = "block";
+        });
+    });
+}
+
+btn__promo.addEventListener("click", () => {
+    thanks.style.display = "none";
+    detail.style.display = "none";
+    consultation.style.display = "none";
+    сalculation.style.display = "block";
+    overlay.style.display = "block";
+    
+});
+
+
+document.querySelector(".question__form").addEventListener("submit", checkForm);
+
+function checkForm(event) {
+    event.preventDefault();
+
+    let el = document.querySelector(".question__form");
+    
+    let name = document.forms["question__form"]["name"].value;
+    let tell = document.forms["question__form"]["tell"].value;
+    let email = document.forms["question__form"]["email"].value;
+    let fail = "";
+
+    if(name == "" || tell == "" || email == "")
+        {fail = "Заполните все поля";}
+    else if(name.length <=1 || name.length > 30)
+        {fail = "Введите корректно имя";}
+    else if(email.split('@').length - 1 == 0 || email.split('.').length - 1 == 0) {
+        fail = "Заполните email корректно";
+    }
+
+    if(fail != ""){
+        // document.querySelector("#error").innerHTML = fail;
+        alert(fail);
+        console.log('w');
+        
+    } else {
+        // alert("Все данние корректно заповнени");
+        console.log('q');
+        return 2;}
+     
+}
+const qw = checkForm;
+console.log(qw);
+   
+
+const slide_left = document.querySelector('.arrow__left');
+const slide_right = document.querySelector('.arrow__right');
+let slides = document.querySelectorAll('.carusel__item');
+    let idx = 0; // Индекс текущего слайда.
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const nextSlide = () => {
+    if(index == slides.length - 1){
+        index = 0;
+        activeSlide(index);
+    } else {
+        index++;
+        activeSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index == 0){
+        index = slides.length - 1;
+        activeSlide(index);
+    } else {
+        index--;
+        activeSlide(index);
+    }
+}
+slide_left.addEventListener('click', prevSlide);
+slide_right.addEventListener('click', nextSlide);
+
+
+//     slide_left.addEventListener('click', moveRight);
+// function moveRight(){
+//     slide_left.style.display = 'block';
+//     carusel__item[idx].style.display = 'none'; // Скрываем текущий слайд
+//     carusel__item[++idx].style.display = 'block'; // Инкрементируем индекс и показываем следующий слайд
+//     if (idx === total_img.length - 1) { // Убираем "правую" стрелку, если справа слайдов больше нет
+//     slide_left.style.display = 'none';
+//     }
+// }
+
+// //Аналогично, только для левой стрелки
+// slide_right.addEventListener('click', moveLeft);
+// function moveLeft(){
+//     slide_right.style.display = 'block';
+//     carusel__item[idx].style.display = 'none';
+//     carusel__item[--idx].style.display = 'block';
+//     if (idx === 0) {
+//         slide_right.style.display = 'none';
+//     }
+// }
+    
+
+
+// $('[data-modal=thanks]').on('click', function() {
+//     $('.overlay, #thanks').fadeIn('slow');
+// });
+// $('[data-modal=consultation]').on('click', function() {
+//     $('.overlay, #consultation').fadeIn('slow');
+// });
+// $('.modal__close').on('click', function() {
+//     $('.overlay, #consultation, #thanks').fadeOut('slow');
+// });
+// $('[data-modal=detail]').each(function(i) {
+//     $(this).on('click', function() {
+//         $('#detail .modal__subtitle').text($('.card__title').eq(i).text())
+//         $('.overlay, #detail').fadeIn('slow');
+//     })
+// })
